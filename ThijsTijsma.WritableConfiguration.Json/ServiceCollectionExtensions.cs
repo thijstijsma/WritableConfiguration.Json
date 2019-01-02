1b941d10e7d8207109c24f38aa108f30d60a43b1
Thijs Tijsma <personal@thijstijsma.nl>
2019-01-02 22:05:03 +0100

    Remove dependency on ASP.NET Core hosting abstractions. (Resolves #1)

--- ThijsTijsma.WritableConfiguration.Json/ServiceCollectionExtensions.cs
+++ ThijsTijsma.WritableConfiguration.Json/ServiceCollectionExtensions.cs
@@ -1,22 +1,21 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
 
 namespace ThijsTijsma.WritableConfiguration.Json
 {
 	public static class ServiceCollectionExtensions
 	{
-		public static void AddWritableJsonConfiguration<T>(this IServiceCollection services, IConfigurationSection configurationSection, string fileName = "appsettings.json") where T : class, new()
+		public static void AddWritableJsonConfiguration<T>(this IServiceCollection services, IFileProvider fileProvider, IConfigurationSection configurationSection, string fileName = "appsettings.json") where T : class, new()
 		{
 			services.Configure<T>(configurationSection);
 
 			services.AddTransient<IWritableOptions<T>>(provider =>
 			{
-				var hostingEnvironment = provider.GetService<IHostingEnvironment>();
 				var optionsMonitor = provider.GetService<IOptionsMonitor<T>>();
 
-				return new WritableOptions<T>(hostingEnvironment, optionsMonitor, configurationSection.Key, fileName);
+				return new WritableOptions<T>(fileProvider, optionsMonitor, configurationSection.Key, fileName);
 			});
 		}
 	}
--- ThijsTijsma.WritableConfiguration.Json/WritableOptions.cs
+++ ThijsTijsma.WritableConfiguration.Json/WritableOptions.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
-using Microsoft.AspNetCore.Hosting;
+
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -8,7 +10,7 @@ namespace ThijsTijsma.WritableConfiguration.Json
 {
 	public class WritableOptions<T> : IWritableOptions<T> where T : class, new()
 	{
-		private readonly IHostingEnvironment _hostingEnvironment;
+		private readonly IFileProvider _fileProvider;
 		private readonly IOptionsMonitor<T> _optionsMonitor;
 		private readonly string _sectionKey;
 		private readonly string _fileName;
@@ -16,9 +18,9 @@ namespace ThijsTijsma.WritableConfiguration.Json
 		public T Value => _optionsMonitor.CurrentValue;
 		public T Get(string name) => _optionsMonitor.Get(name);
 
-		public WritableOptions(IHostingEnvironment hostingEnvironment, IOptionsMonitor<T> optionsMonitor, string sectionKey, string fileName)
+		public WritableOptions(IFileProvider fileProvider, IOptionsMonitor<T> optionsMonitor, string sectionKey, string fileName)
 		{
-			_hostingEnvironment = hostingEnvironment;
+			_fileProvider = fileProvider;
 			_optionsMonitor = optionsMonitor;
 			_sectionKey = sectionKey;
 			_fileName = fileName;
@@ -26,8 +28,7 @@ namespace ThijsTijsma.WritableConfiguration.Json
 
 		public void Write()
 		{
-			var fileProvider = _hostingEnvironment.ContentRootFileProvider;
-			var fileInfo = fileProvider.GetFileInfo(_fileName);
+			var fileInfo = _fileProvider.GetFileInfo(_fileName);
 			var physicalPath = fileInfo.PhysicalPath;
 
 			JObject jObject;
